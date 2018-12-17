700827b069ceb1b11ba875d018f5953d67ea2611
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2018-12-18 00:20:35 +0500

    A complete project ( CMS-in-react-js-Firebase)

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+
+  
+  
+  export const  DB_CONFIG = {
+    apiKey: "AIzaSyABiTV0E-Ty4c8vZ_BfRJW5KSjI9unYKac",
+    authDomain: "content-management-d1965.firebaseapp.com",
+    databaseURL: "https://content-management-d1965.firebaseio.com",
+    projectId: "content-management-d1965",
+    storageBucket: "content-management-d1965.appspot.com",
+    messagingSenderId: "688301581441"
+  };
+  
